af579e1231ac674d96c166acfdc810cae4e935f4
Johan van Eck <johveck@gmail.com>
2016-12-03 17:25:25 +0100

    Add README and some more code improvements

---
+++ README.md
@@ -0,0 +1,2 @@
+# Reachbot
+Reachbot is slack bot for the Reacher project. It can handle JIRA issues and has other functionalities
--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const botController = config['bot-controller'];
 const jira = config.jira;
 const envVariables = config['env-variables'];
 const commands = require('./commands');
+const listenFor = [
+  {
+    regex: '^link',
+    on: ['direct_mention', 'direct_message'],
+    handler: commands.jira.link
+  },
+  {
+    regex: [new RegExp(envVariables('JIRA_ISSUE_REGEX'), 'gi')],
+    on: ['ambient', 'direct_message'],
+    handler: commands.jira.find
+  }
+]
 
-botController.hears("^link", ['direct_mention', 'direct_message'], commands.jira.link);
-botController.hears([new RegExp(envVariables('JIRA_ISSUE_REGEX'), 'gi')], ['ambient', 'direct_message'], commands.jira.find);
+listenFor.forEach((listenItem) => {
+  botController.hears(listenItem.regex, listenItem.on, listenItem.handler);
+});
