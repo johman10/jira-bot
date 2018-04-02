56af7e2c9efd3d655d87de7bbdda5854484e4280
Johan van Eck <johveck@gmail.com>
2018-04-02 11:00:03 +0200

    Remove JIRA_HOST reference

--- config/jira.js
+++ config/jira.js
@@ -2,7 +2,7 @@ const envVariables = require('./env-variables');
 const JiraClient = require('jira-connector');
 
 const jira = new JiraClient({
-  host: envVariables('JIRA_HOST'),
+  host: envVariables('JIRA_URL'),
   basic_auth: {
     username: envVariables('JIRA_USERNAME'),
     password: envVariables('JIRA_PASSWORD'),
