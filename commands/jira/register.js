07d06e6c5d29681f4603f54860c62419a7b29cbb
Johan van Eck <johveck@gmail.com>
2018-04-13 12:42:54 +0200

    Simplfy the registration process

--- commands/jira/register.js
+++ commands/jira/register.js
@@ -1,53 +1,28 @@
-const { URL } = require('url');
 const config = require('../../config');
 
-const envVariables = config['env-variables'];
 const i18n = config.locales;
 
 function registerUser(bot, message) {
-  bot.startConversation(message, (error, conversation) => {
-    const jiraProfileUrl = new URL('secure/ViewProfile.jspa', envVariables('JIRA_URL'));
-    conversation.ask(i18n.t('jira.register.question', { jiraProfileUrl: jiraProfileUrl.toString() }), [
-      {
-        pattern: /"(.*)"/,
-        callback(response) {
-          const userData = { id: response.user, jira_username: response.match[1] };
-          bot.botkit.storage.users.save(userData, (saveError) => {
-            if (saveError) {
-              conversation.say(i18n.t('storage.error.save'));
-              conversation.repeat();
-              conversation.next();
-            }
+  if (message.match.input.match(/(.*?)@(.*?)\.(.*?)/)) {
+    bot.replyInThread(message, i18n.t('jira.register.error.no_email'));
+    return;
+  }
 
-            conversation.say(i18n.t('jira.register.success', { username: response.match[1] }));
-            conversation.next();
-          });
-        },
-      },
-      {
-        pattern: /cancel/,
-        callback() {
-          conversation.say(i18n.t('jira.register.stop'));
-          conversation.next();
-        },
-      },
-      {
-        pattern: /(.*?)@(.*?)\.(.*?)/,
-        callback() {
-          conversation.say(i18n.t('jira.register.error.no_email'));
-          conversation.repeat();
-          conversation.next();
-        },
-      },
-      {
-        default: true,
-        callback() {
-          conversation.say(i18n.t('jira.register.error.no_result'));
-          conversation.repeat();
-          conversation.next();
-        },
-      },
-    ]);
+  const usernameMatch = message.match.input.match(/"(.*)"/);
+  if (!usernameMatch) {
+    bot.replyInThread(message, i18n.t('jira.register.error.no_result'));
+    return;
+  }
+
+  const username = usernameMatch[1];
+  const userData = { id: message.user, jira_username: username };
+  bot.botkit.storage.users.save(userData, (saveError) => {
+    if (saveError) {
+      bot.replyInThread(message, i18n.t('storage.error.save'));
+      return;
+    }
+
+    bot.replyInThread(message, i18n.t('jira.register.success', { username }));
   });
 }
 
